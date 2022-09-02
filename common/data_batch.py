e1201d75768f4d694ace8d7cd39ef38df5427beb
jsm9720 <jsm9720@a.ut.ac.kr>
2022-09-02 08:28:53 +0000

    REFACTORING: Code-Refactoring
    Code refactoring ver 1

--- common/data_batch.py
+++ common/data_batch.py
@@ -4,14 +4,30 @@ from astar_ged.src.distance import ged, normalized_ged
 import multiprocessing as mp
 import pickle
 import random
-import networkx as nx
-import matplotlib.pyplot as plt
 
 
-def make_pkl(dataset, queue, train_num_per_row, max_row_per_worker):   # 64
+def make_pkl(dataset, queue, train_num_per_row, max_row_per_worker, train):
+    '''Make pickle file(create train dataset)
+    Format of one train data is graph1, graph2, ged of graph1 and graph2.
+    This process is to create a train dataset from subgraphs.
+    It creates `max_row_per_worker` train dataset per subgraph.
+    The train dataset have positive and negative dataset at the same rate.
+    The positive dataset is created by generating a new subgraph that removes
+    particular node from the original subgraph.
+    The negative dataset is created by using other subgraphs.
+    And then, it stores the processed train dataset per worker in pickle file
+
+    dataset: Subgraphs generated from scene graph
+    queue: Counter for checking processed subgraphs (rule flag)
+    train_num_per_row: Number of datasets created by one subgraph
+    max_row_per_worker: Number of subgraphs processed by one processor
+    train: Whether it's learning or not (True or False)
+
+    return: list of subgraph1, list of subgraph2, list of ged
+    '''
     g1_list = []
     g2_list = []
-    geds = []
+    ged_list = []
     cnt = 0
     length = len(dataset)
     while True:
@@ -25,49 +41,70 @@ def make_pkl(dataset, queue, train_num_per_row, max_row_per_worker):   # 64
             s = num
             e = len(dataset)
         for i in range(s, e):
-            for _ in range(train_num_per_row):
+            if train:
+                for _ in range(train_num_per_row):
+                    dataset[i].graph['gid'] = 0
+                    # print(i, dataset[i])
+                    if cnt > (train_num_per_row//2):
+                        # print(a, b)
+                        l = list(dataset[i].nodes())
+                        l.remove(random.choice(l))
+                        graph2 = dataset[i].subgraph(l)
+                        # print(1, r)
+                    else:
+                        r = random.randrange(length)
+                        graph2 = dataset[r]
+                    graph2.graph['gid'] = 1
+                    d = ged(dataset[i], graph2, 'astar',
+                            debug=False, timeit=False)
+                    d = normalized_ged(d, dataset[i], graph2)
+                    g1_list.append(dataset[i])
+                    g2_list.append(graph2)
+                    ged_list.append(d)
+                    cnt += 1
+                cnt = 0
+            else:
                 dataset[i].graph['gid'] = 0
-                # print(i, dataset[i])
-                if cnt > (train_num_per_row//2):
-                    # print(a, b)
-                    l = list(dataset[i].nodes())
-                    l.remove(random.choice(l))
-                    graph2 = dataset[i].subgraph(l)
-                    # print(1, r)
-                else:
-                    r = random.randrange(length)
-                    graph2 = dataset[r]
-                graph2.graph['gid'] = 1
-                d = ged(dataset[i], graph2, 'astar',
+                r = random.randrange(length)
+                dataset[r].graph['gid'] = 1
+                d = ged(dataset[i], dataset[r], 'astar',
                         debug=False, timeit=False)
-                d = normalized_ged(d, dataset[i], graph2)
-                print(d)
+                d = normalized_ged(d, dataset[i], dataset[r])
                 g1_list.append(dataset[i])
-                g2_list.append(graph2)
-                geds.append(d)
-                cnt += 1
-            cnt = 0
-        with open("common/data/DB_dataset_ver3_100000/DB_dataset_ver3_100000_1/{}_{}.pickle".format(s, e), "wb") as fw:
-            pickle.dump([g1_list, g2_list, geds], fw)
+                g2_list.append(dataset[r])
+                ged_list.append(d)
+
+        with open("common/data/DB_dataset_ver3_100000/test/{}_{}.pickle".format(s, e), "wb") as fw:
+            pickle.dump([g1_list, g2_list, ged_list], fw)
         g1_list = []
         g2_list = []
-        geds = []
+        ged_list = []
 
 
-def main():
+def main(train):
     mp.set_start_method('spawn')
     q = mp.Queue()
-    train_num_per_row = 64  # 64, 한 image가 비교하는 개수
-    max_row_per_worker = 50  # 50, image 개수
-    number_of_worker = 80  # 80, 프로세서 개수
+    train_num_per_row = 64      # Number of datasets created by one subgraph
+    max_row_per_worker = 64     # Number of Subgraphs processed by one processor
+    number_of_worker = 80       # Number of processor
     with open("data/networkx_ver3_100000/v3_x1000.pickle", "rb") as fr:
         dataset = pickle.load(fr)
     total = []
+    # total_class = set()
     # idx2 = []
-    for i in range(len(dataset)):
-        subs = make_subgraph(dataset[i], 4, False, False)
+    # for i in range(len(dataset)):
+    for i in range(10000):
+        if train:
+            subs = make_subgraph(dataset[i], 4, False, False)
+        else:
+            subs = make_subgraph(dataset[i], 3, False, False)
+            subs = subs[:2]
         # idx2.append(len(subs))
+        # total_class |= {v for idx, v in dataset[i].nodes(data='name')}
         total.extend(subs)
+
+    # print("class 수 :",len(total_class))
+
     # print("각 이미지에 대한 subgraph 수 :", idx2)
     # print("max", max(idx2), "min", min(idx2))
     # print("10개 이상 :", len([i for i in idx2 if 10 < i]))
@@ -77,17 +114,14 @@ def main():
     # print("100개 이상 :", len([i for i in idx2 if 100 < i]))
     # print("총 subgraph 수 :", len(total))
     # exit()
-    for i in range(0, len(total), max_row_per_worker):
-        # for i in range(0, 10, max_row_per_worker): # test
-        q.put(i)
 
-    # print(total[0].nodes.data())
-    # print(type(list(total[0].nodes.data())[0][1]['f1']))
+    for i in range(0, len(total), max_row_per_worker):
+        q.put(i)
 
     workers = []
     for i in range(number_of_worker):
         worker = mp.Process(target=make_pkl, args=(
-            total, q, train_num_per_row, max_row_per_worker))
+            total, q, train_num_per_row, max_row_per_worker, train))
         workers.append(worker)
         worker.start()
 
@@ -96,4 +130,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(True)
---
+++ subgraph_matching/extract.py
@@ -0,0 +1,150 @@
+from common import utils
+from common import models
+from common import subgraph
+from subgraph_matching.config import parse_encoder
+
+from collections import Counter
+
+import os
+import torch
+import argparse
+import pickle
+
+
+def feature_extract(args):
+    ''' Extract feature from subgraphs
+    It extracts all subgraphs feature using a trained model.
+    and then, it compares DB subgraphs and query subgraphs and finds
+    5 candidate DB subgraphs with similar query subgraphs.
+    Finally, it counts all candidate DB subgraphs and finds The highest counted image.
+    '''
+    max_node = 3
+    R_BFS = True
+    ver = 2
+    dataset, db_idx, querys, query_idx = load_dataset(max_node, R_BFS)
+    db_data = utils.batch_nx_graphs(dataset, None)
+    db_data = db_data.to(utils.get_device())
+
+    # model load
+    if not os.path.exists(os.path.dirname(args.model_path)):
+        os.makedirs(os.path.dirname(args.model_path))
+    model = models.OrderEmbedder(1, args.hidden_dim, args)
+    model.to(utils.get_device())
+    if args.model_path:
+        model.load_state_dict(torch.load(
+            args.model_path, map_location=utils.get_device()))
+    else:
+        return print("model does not exist")
+
+    db_check = [{i[1] for i in d.nodes(data="name")}for d in dataset]
+    temp = []
+    results = []
+    candidate_imgs = []
+    model.eval()
+    with torch.no_grad():
+        emb_db_data = model.emb_model(db_data)
+        for i in querys:
+            query = temp.copy()
+            query.append(i)
+            query = utils.batch_nx_graphs(query, None)
+            query = query.to(utils.get_device())
+            emb_query_data = model.emb_model(query)
+            print(emb_db_data.shape)
+            e = torch.sum(torch.abs(emb_query_data - emb_db_data), dim=1)
+            rank = [(i, d) for i, d in enumerate(e)]
+            rank.sort(key=lambda x: x[1])
+            q_check = {n[1] for n in i.nodes(data="name")}
+            print("Q graph nodes :", q_check)
+            print("number of DB subgraph", e.shape)
+            # result = [(query_idx+1, i)]
+            result = []
+            for n, d in rank[:5]:
+                print("DB img id :", db_idx[n]+1)
+                print("similarity : {:.5f}".format(d.item()))
+                print("DB graph nodes :", db_check[n])
+                result.append((db_idx[n]+1, dataset[n]))
+
+                candidate_imgs.append(db_idx[n]+1)
+
+            results.append(result)
+
+            # Check similar/same class count with subgraph in DB
+            checking_in_db = [len(q_check) - len(q_check - i)
+                              for i in db_check]
+            checking_result = Counter(checking_in_db)
+            print(checking_result)
+
+            # Check similar/same class with subgraph in DB
+            value_checking_in_db = [
+                str(q_check - (q_check - i)) for i in db_check]
+            value_checking_result = Counter(value_checking_in_db)
+            print(value_checking_result)
+            print("==="*20)
+    # Final image rank
+    imgs = Counter(candidate_imgs)
+    print(imgs)
+
+    # Store result
+    # if R_BFS:
+    #     with open("plots/data/"+"ver"+str(ver)+"_"+str(query_idx+1)+"_"+str(max_node)+"_RBFS.pickle", "wb") as fr:
+    #         pickle.dump(results, fr)
+    # else:
+    #     with open("plots/data/"+"ver"+str(ver)+"_"+str(query_idx+1)+"_"+str(max_node)+"_dense.pickle", "wb") as fr:
+    #         pickle.dump(results, fr)
+
+
+def load_dataset(max_node, R_BFS):
+    ''' Load subgraphs
+    Load Scene Graph and then, Creat subgraphs from Scene Graphs.
+    First, it reads scene graphs of Visual Genome and then, it makes subgraphs
+    Second, it selects query image and then, it makes subgraphs
+    ps) It can use user-defined query images
+
+    max_node: When subgraphs create, It configures subgraph size.
+    R_BFS: When subgraphs create, Whether it`s R_BFS mothod or not.
+
+    Return
+    db: Subgraphs in database
+    db_idx: Index image of subgraphs
+    query: Query subgraphs/subgraph
+    query_number: Query subgraph number
+    '''
+    with open("data/networkx_ver3_100000/v3_x1000.pickle", "rb") as fr:
+        # with open("data/networkx_ver2_10000.pickle", "rb") as fr:
+        datas = pickle.load(fr)
+
+    db = []
+    db_idx = []
+
+    # Make subgraph from scene graph of Visual Genome
+    query_number = 5002
+    for i in range(len(datas)):
+        if query_number == i:
+            continue
+        subs = subgraph.make_subgraph(datas[i], max_node, False, R_BFS)
+        db.extend(subs)
+        db_idx.extend([i]*len(subs))
+
+    # Select query image
+    # query = subgraph.make_subgraph(datas[query_number], max_node, False, True)
+
+    # user-defined query images
+    with open("data/query_road_0819.pickle", "rb") as q:
+        querys = pickle.load(q)
+        query = subgraph.make_subgraph(querys[0], max_node, False, False)
+        query_number = 1
+    return db, db_idx, query, query_number
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Order embedding arguments')
+
+    utils.parse_optimizer(parser)
+    parse_encoder(parser)
+    args = parser.parse_args()
+
+    feature_extract(args)
+
+
+if __name__ == "__main__":
+    main()
